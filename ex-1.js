d7078a9e440f7551540d606151d42a89c0e44c54
Chanisara <chanisara.sra@gmail.com>
2025-07-22 13:56:09 +0700

    finish chom-ex1&2: implement callback and higher-order functions in exercises

--- ex-1.js
+++ ex-1.js
@@ -2,25 +2,30 @@
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
-  // Start coding here
+  return previousSalary + 5000;   
 }
 
 function forEach(array, operation) {
-  // Start coding here
+  const result = [];
+  for (let i = 0; i < array.length; i=i+1) {
+    result.push(operation(array[i]));
+  }
+  return result;
 }
 
-// Using `forEach` function here
-
-let newEmployeeSalaries;
-
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
 /*
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+  Ans - ฟังก์ชัน addSalary5000 เป็นฟังก์ชั่น Callback
+  มีจุดสังเกตคือ ฟังก์ชันถูกส่งเป็นพารามิเตอร์ให้กับฟังก์ชั่นอื่น และไม่ได้ถูกเรียกใช้เองโดยตรง แต่ถูกเรียกผ่านฟังก์ชั่นอื่น
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+  Ans - ฟังก์ชั่น forEach เป็นฟังก์ชั่น Higher Order Function
+  มีจุดสังเกตคือ ฟังก์ชันนี้รับฟังก์ชันอื่นเป็นพารามิเตอร์
 
 ====================================
 */
